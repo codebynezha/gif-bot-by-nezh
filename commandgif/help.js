9acb399f428edae9f15b97693d1a8c75e8c17326
FiiXxx <84270777+FiiXxx@users.noreply.github.com>
2021-08-08 23:43:18 +0300

    Update help.js

--- commandgif/help.js
+++ commandgif/help.js
@@ -15,13 +15,15 @@ const embed = new Discord.MessageEmbed()
 **Info Commands**
 \&avatar\` - \&about\` - \&ping\`
 \&invite\` - \&server\` - \&support\`
+\&slop\`\&FiiXAbout\'
 
 **Gifs Commands**
 \&boy\` - \&girl\` - \&baby\`
 \&neon\` - \&sad\` - \&smoking\`
 \&animal\` - \&anime\` - \&cuople\`
-\&emoji\`
-
+\&emoji\`\&hack\' \&com\`\&anoymoys\'
+\&snap\`\&dev\'\&ghot\`\&flet\'
+\&rules\`\&welcome\'
 **Links**
 **[Invite](https://discord.com/api/oauth2/authorize?client_id=863719625300639764&permissions=8&scope=bot) - [Server Support](https://discord.gg/nKxJHKPpkK)**
 `)
